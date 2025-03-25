7ea874d2e59168aae899c10c4c3887ba1748965e
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2025-03-25 13:25:41 -0400

    Port setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello world 3000");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker app!");
+});
+
+app.listen(3000, () => {
+  console.log("Subscription Tracker API is running on http://localhost:3000");
+});
+
+export default app;
