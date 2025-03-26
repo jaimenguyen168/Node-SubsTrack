73de8d1876b1f1f7fe2a533d9597aabbca8a70c2
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-03-26 00:46:48 -0400

    Add necessary middlewares

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./configs/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToMongoDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// To enable JSON parsing
+app.use(express.json());
+
+// To enable URL-encoded parsing
+app.use(express.urlencoded({ extended: false }));
+
+// To enable cookie parsing
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker app!");
