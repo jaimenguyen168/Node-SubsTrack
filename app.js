83eb24379915fc0b622f9e2b86e051eb9b1eb4e4
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-03-26 17:25:56 -0400

    Add rate limit with arcjet

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToMongoDB from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
@@ -18,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // To enable cookie parsing
 app.use(cookieParser());
+
+// To enable Arcjet
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
--- configs/env.js
+++ configs/env.js
@@ -4,5 +4,5 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV } = process.env;
+
